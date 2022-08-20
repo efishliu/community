df9c734c15c42b462d4c59dcb3246d88ea5e633a
liugang18 <liugang18@baidu.com>
2022-08-20 20:41:54 +0800

    first commit in 2022-08-20

---
+++ src/main/java/com/nowcoder/community/config/SecurityConfig.java
@@ -0,0 +1,129 @@
+package com.nowcoder.community.config;
+
+import com.nowcoder.community.entity.LoginTicket;
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.UserService;
+import com.nowcoder.community.util.CommunityConstant;
+import com.nowcoder.community.util.CommunityUtil;
+import com.nowcoder.community.util.CookieUtil;
+import com.nowcoder.community.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextImpl;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter implements CommunityConstant {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/resources/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 授权
+        http.authorizeRequests()
+                .antMatchers(
+                        "/user/setting",
+                        "/user/upload",
+                        "/discuss/add",
+                        "/comment/add/**",
+                        "/letter/**",
+                        "/notice/**",
+                        "/like",
+                        "/follow",
+                        "/unfollow"
+                )
+                .hasAnyAuthority(
+                        AUTHORITY_USER,
+                        AUTHORITY_ADMIN,
+                        AUTHORITY_MODERATOR
+                )
+                .anyRequest().permitAll()
+                .and().csrf().disable();
+
+        // 权限不够时的处理
+        http.exceptionHandling()
+                .authenticationEntryPoint(new AuthenticationEntryPoint() {
+                    // 没有登录
+                    @Override
+                    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException e) throws IOException, ServletException {
+                        String xRequestedWith = request.getHeader("x-requested-with");
+                        if ("XMLHttpRequest".equals(xRequestedWith)) {
+                            response.setContentType("application/plain;charset=utf-8");
+                            PrintWriter writer = response.getWriter();
+                            writer.write(CommunityUtil.getJSONString(403, "你还没有登录哦!"));
+                        } else {
+                            String ticket = CookieUtil.getValue(request, "ticket");
+                            if (ticket != null) {
+                                // 查询凭证
+                                LoginTicket loginTicket = userService.findLoginTicket(ticket);
+                                // 检查凭证是否有效
+                                //有效
+                                if (loginTicket != null && loginTicket.getStatus() == 0 && loginTicket.getExpired().after(new Date())) {
+                                    //response.sendRedirect(request.getContextPath() + "/login");
+                                    User user = userService.findUserById(loginTicket.getUserId());
+                                    // 在本次请求中持有用户
+                                    hostHolder.setUser(user);
+                                    // 构建用户认证的结果,并存入SecurityContext,以便于Security进行授权.
+                                    Authentication authentication = new UsernamePasswordAuthenticationToken(
+                                            user, user.getPassword(), userService.getAuthorities(user.getId()));
+                                    SecurityContextHolder.setContext(new SecurityContextImpl(authentication));
+                                    response.sendRedirect(request.getRequestURI());
+
+                                }else{//无效
+                                    //response.sendRedirect(request.getContextPath() + "/logout");
+                                    response.sendRedirect(request.getContextPath() + "/login");
+                                }
+                            }else{
+                                response.sendRedirect(request.getContextPath() + "/login");
+                            }
+                        }
+                    }
+                })
+                .accessDeniedHandler(new AccessDeniedHandler() {
+                    // 权限不足
+                    @Override
+                    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException e) throws IOException, ServletException {
+                        String xRequestedWith = request.getHeader("x-requested-with");
+                        if ("XMLHttpRequest".equals(xRequestedWith)) {
+                            response.setContentType("application/plain;charset=utf-8");
+                            PrintWriter writer = response.getWriter();
+                            writer.write(CommunityUtil.getJSONString(403, "你没有访问此功能的权限!"));
+                        } else {
+                            response.sendRedirect(request.getContextPath() + "/denied");
+
+                        }
+                    }
+                });
+
+        // Security底层默认会拦截/logout请求,进行退出处理.
+        // 覆盖它默认的逻辑,才能执行我们自己的退出代码.
+        http.logout().logoutUrl("/securitylogout");
+    }
+
+}
---
+++ src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
@@ -0,0 +1,24 @@
+package com.nowcoder.community.dao;
+
+import com.nowcoder.community.entity.DiscussPost;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface DiscussPostMapper {
+
+    List<DiscussPost> selectDiscussPosts(int userId, int offset, int limit);
+
+    // @Param注解用于给参数取别名,
+    // 如果只有一个参数,并且在<if>里使用,则必须加别名.
+    int selectDiscussPostRows(@Param("userId") int userId);
+
+    int insertDiscussPost(DiscussPost discussPost);
+
+    DiscussPost selectDiscussPostById(int id);
+
+    int updateCommentCount(int id, int commentCount);
+
+}
